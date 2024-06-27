5ccff932af42dfb2522af48ca52bab68ea13ea2f
Melissa J. Chin <melissa1jchin@gmail.com>
2024-06-27 16:41:26 -0500

    created seed data arrays

---
+++ server/db/seedData.js
@@ -0,0 +1,49 @@
+const arrivals = [
+  {
+    travelerName: "Melissa",
+    travelDate: "08/16/2024",
+    tripNumber: "TP 244",
+    travelOrigin: "Chicago, IL",
+    departureTime: "6:00pm",
+    travelDestination: "Lisbon, Portugal",
+    arrivalTime: "7:55am",
+  },
+];
+
+const departures = [
+  {
+    travelerName: "Melissa",
+    travelDate: "08/23/2024",
+    tripNumber: "TP 243",
+    travelOrigin: "Lisbon, Portugal",
+    departureTime: "12:40pm",
+    travelDestination: "Chicago, IL",
+    arrivalTime: "4:00pm",
+  },
+];
+
+const activities = [
+  {
+    activityName: "",
+    activityDescription: "",
+    activityPhoto: "",
+    activityWebsite: "",
+  },
+];
+
+const calendar = [
+  {
+    eventName: "",
+    eventDate: "",
+    eventTime: "",
+    eventDescription: "",
+    eventWebsite: "",
+  },
+];
+
+const users = [
+  {
+    name: "",
+    password: "",
+  },
+];
