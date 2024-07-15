bb85863e49cf51ced71dd104c6fcd6045c79f9d5
Melissa J. Chin <melissa1jchin@gmail.com>
2024-07-15 15:09:49 -0500

    adding api route files

---
+++ server/api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+router.use("/activities", require("./activities"));
+
+router.use("/arrivals", require("./arrivals"));
+
+router.use("/calendar", require("./calendar"));
+
+router.use("/depatures", require("./departures"));
+
+router.use("/trips", require("./trips"));
+
+router.use("/users", require("./users"));
+
+module.exports = router;
