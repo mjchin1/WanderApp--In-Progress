0c649c04a98259ebdc26a34d220fc018c2a49385
Melissa J. Chin <melissa1jchin@gmail.com>
2024-07-16 15:00:21 -0500

    departures api routes

---
+++ server/api/departures.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getDepartureById,
+  getAllDeparturesByTripId,
+  createDeparture,
+  deleteDeparture,
+  updateDeparture,
+} = require("../db/sqlHelperFunctions/departures");
+
+router.get("/trip/:id", async (req, res, next) => {
+  try {
+    const departures = await getAllDeparturesByTripId(req.params.id);
+    res.send(departures);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/:id", async (req, res, next) => {
+  try {
+    const departure = await createDeparture(req.body);
+    res.send(departure);
+  } catch (error) {
+    next(error);
+    throw new Error(`${error.message}`);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const departure = await getDepartureById(req.params.id);
+    res.send(departure);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const departure = await deleteDeparture(req.params.id);
+    res.send(departure);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const departure = await updateDeparture(req.params.id, req.body);
+    res.send(departure);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
