8dde12cbac3624b3b68e717b73fde79058c3c41c
Melissa J. Chin <melissa1jchin@gmail.com>
2024-08-19 16:36:10 -0500

    building add arrival form

--- client/src/components/AddArrivalForm.jsx
+++ client/src/components/AddArrivalForm.jsx
@@ -1,6 +1,48 @@
-export default function AddArrivalForm() {
-  
-  return (
+import { useState } from 'react';
 
+export default function AddArrivalForm () {
+  const [travelerName, setTravelerName] = useState("");
+  const [travelDate, setTravelDate] = useState("");
+  const [tripNumber, setTripNumber] = useState("");
+  const [travelOrigin, setTravelOrigin] = useState("");
+  const [departureTime, setDepartureTime] = useState("");
+  const [travelDestination, setTravelDestination] = useState("");
+  const [arrivalTime, setArrivalTime] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8080/api/activities/create', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+          travelerName,
+          travelDate,
+          tripNumber,
+          travelOrigin,
+          departureTime,
+          travelDestination,
+          arrivalTime,
+        })
+      });
+      const result = await response.json();
+      setTravelerName("");
+      setTravelDate("");
+      setTripNumber("");
+      setTravelOrigin("");
+      setDepartureTime("");
+      setTravelDestination("");
+      setArrivalTime("");
+    } catch (error) {
+    }
+
+  }
+
+  return (
+    <>
+    
+    </>
   );
 };
