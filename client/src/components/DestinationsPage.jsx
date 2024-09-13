59071a401034e506fad4c6740b75efbf8e20f691
Melissa J. Chin <melissa1jchin@gmail.com>
2024-09-13 12:11:29 -0500

    added additional button skeletons, changed destination button background color and border

--- client/src/components/DestinationsPage.jsx
+++ client/src/components/DestinationsPage.jsx
@@ -22,6 +22,73 @@ function DestinationsPage() {
        <img className="destinationButtonPic" src="https://cdn.aarp.net/content/dam/aarp/travel/budget_travel/2022/06/1140-big-ben-hero.jpg"></img> 
       </button>
 
+      <button className="destinationButton" onClick={() => {
+        setDestination("Paris");
+        setDestinationPic("");
+        console.log(destination)
+      }}>
+       <h3>Paris</h3>
+       <img className="destinationButtonPic" src=""></img> 
+      </button>
+
+      <button className="destinationButton" onClick={() => {
+        setDestination("Copenhagen");
+        setDestinationPic("");
+        console.log(destination)
+      }}>
+       <h3>Copenhagen</h3>
+       <img className="destinationButtonPic" src=""></img> 
+      </button>
+
+      <button className="destinationButton" onClick={() => {
+        setDestination("Zurich");
+        setDestinationPic("");
+        console.log(destination)
+      }}>
+       <h3>Zurich</h3>
+       <img className="destinationButtonPic" src=""></img> 
+      </button>
+
+      <button className="destinationButton" onClick={() => {
+        setDestination("Cartagena");
+        setDestinationPic("");
+        console.log(destination)
+      }}>
+       <h3>Cartagena</h3>
+       <img className="destinationButtonPic" src=""></img> 
+      </button>
+
+      <button className="destinationButton" onClick={() => {
+        setDestination("Lisbon");
+        setDestinationPic("");
+        console.log(destination)
+      }}>
+       <h3>Lisbon</h3>
+       <img className="destinationButtonPic" src=""></img> 
+      </button>
+
+      <button className="destinationButton" onClick={() => {
+        setDestination("Rome");
+        setDestinationPic("");
+        console.log(destination)
+      }}>
+       <h3>Rome</h3>
+       <img className="destinationButtonPic" src=""></img> 
+      </button>
+
+      <button className="destinationButton" onClick={() => {
+        setDestination("Tulum");
+        setDestinationPic("");
+        console.log(destination)
+      }}>
+       <h3>Tulum</h3>
+       <img className="destinationButtonPic" src=""></img> 
+      </button>
+
+
+
+
+
     </> )
 }
 
