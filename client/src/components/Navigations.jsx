2b355ba28ba7db376c0aeddbdbde6b67205a5b98
Melissa J. Chin <melissa1jchin@gmail.com>
2024-10-29 15:41:26 -0500

    commenting out nav bar routes

--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -4,7 +4,7 @@ export default function Navigations() {
     return (
         <nav className="navBar">
             <Link to = '/'>Home</Link>
-            <Link to = '/activities'>Activities</Link>
+            {/* <Link to = '/activities'>Activities</Link>
             <Link to = '/arrivals'>Arrivals</Link>
             <Link to = '/departures'>Departures</Link>
             <Link to = '/add-arrival'>Arrival Form</Link>
@@ -13,7 +13,7 @@ export default function Navigations() {
             <Link to = '/add-trip'>Add Trip Form</Link>
             <Link to = '/destinations'>Destinations</Link>
             <Link to = '/trip/:id'>Single Trip</Link>
-            <Link to = '/activity-types'>Activity Types</Link>
+            <Link to = '/activity-types'>Activity Types</Link> */}
 
         </nav>
     );
