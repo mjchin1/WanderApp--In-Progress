74ca8204ed6896bfbcda4e155ae2de56bb9c8595
Melissa J. Chin <melissa1jchin@gmail.com>
2024-11-19 16:48:25 -0600

    reformmating individual arrival info

--- client/src/components/SingleArrival.jsx
+++ client/src/components/SingleArrival.jsx
@@ -38,10 +38,12 @@ function SingleArrival({ arrivals, setArrivals, arrival, setArrival }) {
       <div className="arrivalContainer">
           <div key={arrival.arrival_id} className="arrivalCard">
             <div className="arrivalDetails">
-              {/* <p className="arrivalInfo">{arrival.traveler_name}</p> */}
-              <p className="arrivalInfo">Flight Number: {arrival.trip_number}</p>
-              <p className="arrivalInfo">Leaves {arrival.travel_origin} on {arrival.travel_date} at {arrival.departure_time} </p>
-              <p className="arrivalInfo"> Arrives in {arrival.travel_destination} on {arrival.arrival_date} at {arrival.arrival_time} </p>
+            <p className="arrivalInfo arrivalHeading">{arrival.traveler_name}</p>
+              <p className="arrivalInfo">Flight/Travel Number: {arrival.trip_number}</p>
+              <p className="arrivalInfo">Departure City: {arrival.travel_origin}</p>
+              <p className="arrivalInfo">Arrival Date/Time: {arrival.travel_date} at {arrival.arrival_time}</p>
+              <p className="arrivalInfo">Destination City: {arrival.travel_destination}</p>
+              <p className="arrivalInfo">Arrival Date/Time: {arrival.arrival_date} at {arrival.arrival_time}</p>
             </div>
           </div>
       </div> <br/>
