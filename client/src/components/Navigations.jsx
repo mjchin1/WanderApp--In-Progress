517c5ece9cecf5b4bec74d3eabc504c886ed840a
Melissa J. Chin <melissa1jchin@gmail.com>
2025-02-12 16:27:22 -0600

    updating nav bar

--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -7,16 +7,17 @@ export default function Navigations() {
             {/* <Link to = '/activities'>Activities</Link> */}
             {/* <Link to = '/arrivals'>Arrivals</Link> */}
             {/* <Link to = '/departures'>Departures</Link> */}
-            <Link to = '/add-arrival'>Arrival Form</Link>
-            <Link to = '/add-departure'>Departure Form</Link>
+            <Link to = '/trips'>My Trips</Link>
+            {/* <Link to = '/add-arrival'>Arrival Form</Link>
+            <Link to = '/add-departure'>Departure Form</Link> */}
             {/* <Link to = '/place-search2'>Search2</Link> */}
-            <Link to = '/place-search'>Search</Link>
-            <Link to = '/add-activity'>Activity Form</Link>
+            {/* <Link to = '/place-search'>Search</Link>
+            <Link to = '/add-activity'>Activity Form</Link> */}
             {/* <Link to = '/place-photo'>Photo</Link>
             <Link to = '/place-photo2'>Photo2</Link> */}
             {/* <Link to = '/add-activity'>Activity Form</Link> */}
             {/* <Link to = '/add-trip'>Add Trip Form</Link> */}
-            <Link to = '/destinations'>Destinations</Link>
+            {/* <Link to = '/destinations'>Destinations</Link> */}
             {/* <Link to = '/trip/:id'>Single Trip</Link> */}
             {/* <Link to = '/activity-types'>Activity Types</Link> */}
 
